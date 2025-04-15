0c6a057d4a45886018a7bb8fa7edc6a1b1bed528
Denisse Vargas <denissev102@gmail.com>
2025-04-15 19:30:58 -0400

    ufc

---
+++ ufc.js
@@ -0,0 +1,8 @@
+function quote(fighter) {
+    let winner = fighter.toLowerCase()
+    if (winner == "george saint pierre") {
+      return "I am not impressed by your performance."
+    } else if (winner  == "conor mcgregor") {
+      return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    }
+  };
