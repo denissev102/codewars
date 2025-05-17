80a6e4feb0ce8d7cb676232cc5a6d25f0864cc6a
Denisse Vargas <denissev102@gmail.com>
2025-05-16 23:37:24 -0400

    Find the Capitals

---
+++ findthecapitals.js
@@ -0,0 +1,8 @@
+var capitals = function (word) {
+    let output = []
+      for (let i = 0; i < word.length; i++) {
+      if (word[i] === word[i].toUpperCase()) {
+        output.push(i)
+      }
+    } return output
+  };
