207df67fc2db461333e22fea1be2695a9e24cd2f
Denisse Vargas <denissev102@gmail.com>
2025-10-19 13:37:04 -0400

    BMI Calc

---
+++ bmicalc.js
@@ -0,0 +1,12 @@
+function bmi(weight, height) {
+    let result = weight/height**2;
+    if (result <= 18.5) {
+      return "Underweight";
+    } else if (result <= 25.0) {
+      return "Normal";
+    } else if (result <= 30.0) {
+      return "Overweight";
+    } else (result > 30) 
+      return "Obese";
+      return result
+  }
